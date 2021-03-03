9a3d8875d069d07555ffee0f77808b70e64ef573
Practicum <dosker6@gmail.com>
2021-03-03 11:40:05 +0300

    11

--- polnii.py
+++ polnii.py
@@ -1,2 +1,21 @@
 import math
-import time
+import time #полная хуйня
+go to dick
+gjkysq
+gfdgdf
+dfh
+dfhfd
+hdf
+h
+df
+hash(df
+     h
+df
+hash(df
+     hdf
+h
+df
+hash(df
+     hash(dfh
+          df
+h))))
